3ba9fc50d326dfa31a80b669b142e51927ff05fe
MaduhSamp <madusampietro@gmail.com>
2024-05-20 17:41:48 -0300

    Modificações

--- projeto.php
+++ projeto.php
@@ -1,125 +1,161 @@
 <?php
 
-class User {
-    public $username;
-    public $password;
-    
-    public function __construct($username, $password) {
-        $this->username = $username;
-        $this->password = $password;
-    }
-}
+session_start();
 
-class Sale {
-    public $itemName;
-    public $value;
-    public $time;
-
-    public function __construct($itemName, $value) {
-        $this->itemName = $itemName;
-        $this->value = $value;
-        $this->time = date('d/m/Y H:i:s');
-    }
-}
-
-$users = [
-    new User("admin", "admin123")
+$usuarios = [
+    "Admin" => "admin",
 ];
-$currentUsername = null;
-$sales = [];
 
-function addUser(&$users, $username, $password) {
-    $users[] = new User($username, $password);
-    logMessage("User $username cadastrado");
+$log = [];
+
+$itens = [];
+
+$total_vendas = 0;
+
+function registrar(){
+    global $usuarios;
+    limpar_tela();
+    $nome_usuario = readline("Digite o nome de usuário: ");
+    $senha_usuario = readline("Digite uma senha: ");
+    $usuarios[$nome_usuario] = $senha_usuario;
+    echo "Usuário " . $nome_usuario ." registrado com sucesso!\n";
+    sleep(2);
 }
 
-function login(&$currentUsername, $username, $password) {
-    global $users;
-    foreach ($users as $user) {
-        if ($user->username === $username && $user->password === $password) {
-            $currentUsername = $username;
-            logMessage("Login realizado pelo usuário $username");
-            return true;
+function login(){
+    limpar_tela();
+    global $usuarios;
+    echo "LOGIN\n";
+    $nome_usuario = readline("Nome de usuário: ");
+    $senha_usuario = readline("Senha: ");
+    
+    if (isset($usuarios[$nome_usuario]) && $usuarios[$nome_usuario] == $senha_usuario) {
+        $_SESSION['nome_usuario'] = $nome_usuario;
+        return true;
+    } else {
+        return false;
+    } 
+}
+
+function logout(){
+    global $total_vendas;
+
+    $total_vendas = 0;
+    
+    limpar_tela();
+    session_unset();
+    echo "Logout realizado com sucesso!\n";
+    sleep(2);
+}
+
+function limpar_tela(){
+    system('clear');
+}
+function visualizar_log(){
+    global $log;
+    limpar_tela();
+    echo "Log do Sistema\n";
+    foreach ($log as $entrada) {
+        echo $entrada . "\n";
+    }
+    
+    readline("Pressione Enter para continuar...");
+}
+
+function adicionar_item(){
+    global $itens;
+    limpar_tela();
+    echo "Nome do Item: ";
+    $nome_item = readline();
+    echo "Preço: ";
+    $preco_item = readline();
+    
+    $itens[] = [
+        'nome' => $nome_item,
+        'preco' => $preco_item,
+    ];
+    echo "Item " . $nome_item . " adicionado com sucesso!\n";
+}
+
+function realizar_venda() {
+    global $total_vendas, $itens, $log;
+    limpar_tela();
+
+    if (count($itens) == 0) {
+        echo "Não há itens disponíveis, cadastre um antes de tentar realizar uma venda novamente!\n";
+        readline("Pressione Enter para continuar...");
+        adicionar_item();
+    } 
+    
+    echo "--------------------------\n";
+    echo "Itens disponíveis:\n";
+    foreach ($itens as $item) {
+        echo $item['nome'] . " - R$ " . $item['preco'] . "\n";
+    }
+
+    $item_escolhido = readline("Digite o nome do item que deseja vender: ");
+
+    $item_encontrado = false;
+
+    foreach ($itens as $key => $item) {
+        if ($item['nome'] == $item_escolhido) {
+            $total_vendas += $item['preco'];
+            unset($itens[$key]);
+            $item_encontrado = true;
+            break;
         }
     }
-    return false;
-}
 
-function logout(&$currentUsername) {
-    if ($currentUsername !== null) {
-        logMessage("Usuário $currentUsername deslogou");
-        $currentUsername = null;
+    if ($item_encontrado) {
+        echo "Venda realizada com sucesso!\n";
+    } else {
+        echo "Item não encontrado, tente novamente!\n";
     }
+    $log[] = "Venda realizada em tal hora!";
+    sleep(2);
 }
 
-function sellItem($itemName, $value) {
-    global $currentUsername, $sales;
-    if ($currentUsername !== null) {
-        $sales[] = new Sale($itemName, $value);
-        logMessage("Usuário $currentUsername realizou uma venda do item $itemName no valor de $value");
+
+function menu(){
+    global $total_vendas;
+    limpar_tela();
+    echo "Bem vindo(a) ao mercadinho do dev!\n";
+    echo "Total de Vendas: R$ " . $total_vendas . "\n";
+    echo "1 - Realizar Venda\n";
+    echo "2 - Registrar Novo Usuário\n";
+    echo "3 - Visualizar Log\n";
+    echo "4 - Adicionar Item\n";
+    echo "5 - Logout\n";
+    $opcao = readline("Digite o número correspondente da opção que deseja: ");
+
+    switch ($opcao) {
+        case 1:
+            realizar_venda();
+            break;
+        case 2: 
+            registrar();
+            break;
+        case 3:
+            visualizar_log();
+            break;
+        case 4:
+            adicionar_item();
+            break;
+        case 5: 
+            logout();
+            break;
+        default:
+            echo "Opção inválida!\n";
     }
-}
-
-function showLog() {
-    global $sales;
-    foreach ($sales as $sale) {
-        echo "Usuário realizou uma venda do item {$sale->itemName} no valor de {$sale->value} às {$sale->time}" . PHP_EOL;
-    }
-}
-
-function getTotalSales() {
-    global $sales;
-    $total = 0;
-    foreach ($sales as $sale) {
-        $total += $sale->value;
-    }
-    return $total;
-}
-
-function logMessage($message) {
-    echo date('d/m/Y H:i:s') . " - $message" . PHP_EOL;
-}
+} 
 
 while (true) {
-    if ($currentUsername === null) {
-        echo "Realize o login:\n";
-        $username = readline("Usuário: ");
-        $password = readline("Senha: ");
-        if (login($currentUsername, $username, $password)) {
-            echo "Login realizado com sucesso!\n";
-        } else {
-            echo "Usuário ou senha incorretos. Tente novamente.\n";
+    if (!isset($_SESSION['nome_usuario'])) {
+        if (!login()) {
+            echo "Login falhou, tente novamente!\n";
+            readline("Pressione Enter para continuar...");
         }
     } else {
-        echo "Bem-vindo, $currentUsername!\n";
-        echo "Opções disponíveis:\n";
-        echo "1. Vender\n";
-        echo "2. Cadastrar novo usuário\n";
-        echo "3. Verificar log\n";
-        echo "4. Deslogar\n";
-        echo "Valor total das vendas: " . getTotalSales() . "\n";
-        $option = readline("Escolha uma opção: ");
-        switch ($option) {
-            case '1':
-                $itemName = readline("Nome do item vendido: ");
-                $value = floatval(readline("Valor da venda: "));
-                sellItem($itemName, $value);
-                break;
-            case '2':
-                $newUsername = readline("Novo usuário: ");
-                $newPassword = readline("Nova senha: ");
-                addUser($users, $newUsername, $newPassword);
-                break;
-            case '3':
-                echo "Histórico de log:\n";
-                showLog();
-                break;
-            case '4':
-                logout($currentUsername);
-                break;
-            default:
-                echo "Opção inválida. Tente novamente.\n";
-                break;
-        }
+        menu();
     }
 }
